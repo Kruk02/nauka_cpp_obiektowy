2d28845c60a45d8a4824ed579a281b6309f51af9
Grzegorz Kruk <krukg02@gmail.com>
2025-04-13 10:27:24 +0200

    polimorfizm 1

---
+++ polimorfizm 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+class Pojazd {
+public:
+    virtual void jedz() {
+        cout << "Pojazd jedzie powoli\n";
+    }
+};
+
+class Samochod : public Pojazd {
+public:
+    void jedz() override {
+        cout << "Samochód jedzie szybko\n" << endl;
+    }
+};
+
+int main() {
+    Pojazd* wskaznik = new Samochod();
+    wskaznik->jedz();
+
+    delete wskaznik;
+    return 0;
+}
